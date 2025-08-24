efc4d948fb66f96b4ed2add66e1c69b8210df90c
Jeremy Opriasa <opriasajbusiness@gmail.com>
2025-08-24 16:17:40 +0800

    fix: complete project of caesar cipher encryption

--- Learn string manipulation by building a cipher/ceasar_cipher.py
+++ Learn string manipulation by building a cipher/ceasar_cipher.py
@@ -1,22 +1,29 @@
 text = 'freeCodeCamp'
 shift = 3
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-encrypted_text = ''
 
-# Loop the string text and convert them into lowercase
-for char in text.lower():
-    # make condition if the current character is space
-    if char == ' ':
-        encrypted_text += char
-    else:
-        # find in the alphabet the current char and store to index
-        index = alphabet.find(char)
+# initiate function
+def caesar(message, offset):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    encrypted_text = ''
 
-        # current index plus shift, and use modulo operator to get the remainder of the length of alphabet
-        new_index = (index + shift) % len(alphabet)
+    # Loop the string text and convert them into lowercase
+    for char in message.lower():
+        # make condition if the current character is space
+        if char == ' ':
+            encrypted_text += char
+        else:
+            # find in the alphabet the current char and store to index
+            index = alphabet.find(char)
 
-        # store in the encrypted text the current encrypted alphabet
-        encrypted_text += alphabet[new_index]
+            # current index plus shift, and use modulo operator to get the remainder of the length of alphabet
+            new_index = (index + offset) % len(alphabet)
 
-# Outside loop, print the complete encryption of text
-print('Encrypted text:', encrypted_text)
+            # store in the encrypted text the current encrypted alphabet
+            encrypted_text += alphabet[new_index]
+
+    print('Plain text:', message)
+    # Outside loop, print the complete encryption of text
+    print('Encrypted text:', encrypted_text)
+
+# Call the function
+caesar(text, shift)
